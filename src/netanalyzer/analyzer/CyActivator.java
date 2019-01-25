9338c82a901d2b10ff70313c34c02f2d4211c96c
Adam Treister <adam.treister@gladstone.ucsf.edu>
2019-01-25 12:54:43 -0800

    register ResultsPanel in CyActivator

--- src/netanalyzer/analyzer/CyActivator.java
+++ src/netanalyzer/analyzer/CyActivator.java
@@ -44,8 +44,13 @@ import java.util.Map;
 import java.util.Properties;
 
 import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.application.events.SetCurrentNetworkListener;
 import org.cytoscape.application.swing.CyAction;
 import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.application.swing.CytoPanel;
+import org.cytoscape.application.swing.CytoPanelComponent;
+import org.cytoscape.application.swing.CytoPanelName;
+import org.cytoscape.application.swing.CytoPanelState;
 import org.cytoscape.service.util.AbstractCyActivator;
 import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.view.model.CyNetworkViewManager;
@@ -63,41 +68,54 @@ public class CyActivator extends AbstractCyActivator {
 	public void start(BundleContext bc) {
         final CyServiceRegistrar registrar = getService(bc, CyServiceRegistrar.class);
         final CySwingApplication desktop = getService(bc, CySwingApplication.class);
-		CyApplicationManager cyApplicationManagerServiceRef = getService(bc,CyApplicationManager.class);
-		CyNetworkViewManager viewManagerServiceRef = getService(bc,CyNetworkViewManager.class);
-		final AnalyzerManager manager = new AnalyzerManager(registrar);
+		CyApplicationManager appMgr = getService(bc,CyApplicationManager.class);
+		CyNetworkViewManager viewMgr = getService(bc,CyNetworkViewManager.class);
+		final AnalyzerManager manager = new AnalyzerManager(registrar);		
+
+		Properties props = new Properties();
 		
-		Map<String,String> props = new HashMap<String, String>();
-		props.put(ID, "analyzeNetworkAction");
-		props.put(TITLE, "Analyze Network");
-		props.put(PREFERRED_MENU,"Tools");
-		props.put(MENU_GRAVITY,"9.0");
-		props.put(IN_TOOL_BAR, "false");
-		props.put(ENABLE_FOR, "network");
+		{	// create and register the task factory to be able to run the analysis
+			props.clear();
+//	??? Do we need the AnalyzeNetworkAction below if we assign the menu props to the taskFactory?
+//			props.put(ID, "analyzeNetworkTaskFactory");	
+//			props.put(TITLE, "Analyze Network");
+//			props.put(PREFERRED_MENU,"Tools");
+//			props.put(MENU_GRAVITY,"9.0");
+//			props.put(IN_TOOL_BAR, "false");
+//			props.put(ENABLE_FOR, "network");
+			props.put(COMMAND_NAMESPACE, "analyzer");
+			props.put(COMMAND, "analyze");
+			props.put(COMMAND_DESCRIPTION,  "Calculate statistics on the current network");
+			props.put(COMMAND_LONG_DESCRIPTION, "Run algorithms to calculate a set of statistics on the network, and write those statistics to the node and network tables.");
+			props.put(COMMAND_EXAMPLE_JSON, "{}");
+			props.put(COMMAND_SUPPORTS_JSON, "true");
 
-		props.put(COMMAND_NAMESPACE, "analyzer");
-		props.put(COMMAND, "analyze");
-		props.put(COMMAND_DESCRIPTION,  "Calculate network statistics");
-		props.put(COMMAND_LONG_DESCRIPTION, "Run algorithms to calculate a set of statistics on the network, and write those statistics to the node and network tables.");
-		props.put(COMMAND_EXAMPLE_JSON, "");
-		props.put(COMMAND_SUPPORTS_JSON, "true");
-
-		AnalyzeNetworkAction analyzeNetworkAction = new AnalyzeNetworkAction(cyApplicationManagerServiceRef,viewManagerServiceRef, props, manager, desktop);
-		AnalyzeNetworkTaskFactory analyzeNetworkTaskFactory = new AnalyzeNetworkTaskFactory(registrar, desktop );
-//		registerAllServices(bc,analyzeNetworkTaskFactory, analyzeNetworkAction, new Properties());
-		registerService(bc,analyzeNetworkAction,CyAction.class, new Properties());
-		registerService(bc,analyzeNetworkTaskFactory,AnalyzeNetworkTaskFactory.class);
-
-
-		{
-			ShowResultsPanelTaskFactory results = new ShowResultsPanelTaskFactory(manager);
-			Properties props1 = new Properties();
-			props1.put(TITLE, "Show Results Panel");
-			props1.put(PREFERRED_MENU, "Tools");
-			props1.setProperty(IN_TOOL_BAR, "FALSE");
-			props1.setProperty(IN_MENU_BAR, "TRUE");
-			manager.registerService(results, TaskFactory.class, props1);
+			AnalyzeNetworkTaskFactory analyzeNetworkTaskFactory = new AnalyzeNetworkTaskFactory(registrar, desktop );
+			registerService(bc,analyzeNetworkTaskFactory, AnalyzeNetworkTaskFactory.class, props);
 		}
 
+		{	// create and register the results panel, 
+			// and listen for network change events, so we always show the current network stats
+			ResultsPanel resultsPanel = new ResultsPanel(manager);
+			registerService(bc, resultsPanel, CytoPanelComponent.class);
+			registerService(bc, resultsPanel, SetCurrentNetworkListener.class);
+			CytoPanel panel = desktop.getCytoPanel(CytoPanelName.EAST);
+			panel.setState(CytoPanelState.DOCK);
+		}
+
+		{	// create and register the action to be added to the Tools menu
+			props.clear();
+			props.put(ID, "analyzeNetworkAction");
+			props.put(TITLE, "Analyze Network");
+			props.put(PREFERRED_MENU,"Tools");
+			props.put(MENU_GRAVITY,"9.0");
+			props.put(IN_TOOL_BAR, "false");
+			props.put(ENABLE_FOR, "network");
+	
+			Map<String, String> map = (Map)props;
+			AnalyzeNetworkAction analyzeNetworkAction = new AnalyzeNetworkAction(appMgr,viewMgr, map, manager, desktop);
+			registerService(bc,analyzeNetworkAction,CyAction.class, props);
+		}
+		
 	}
 }
--- src/netanalyzer/analyzer/NetAnalyzerAction.java
+++ src/netanalyzer/analyzer/NetAnalyzerAction.java
@@ -1,37 +1,11 @@
 package analyzer;
 
-/*
- * #%L
- * Cytoscape NetworkAnalyzer Impl (network-analyzer-impl)
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2006 - 2013
- *   Max Planck Institute for Informatics, Saarbruecken, Germany
- *   The Cytoscape Consortium
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-2.1.html>.
- * #L%
- */
-
 import java.util.Map;
 
-import org.cytoscape.model.CyNetwork;
-import org.cytoscape.view.model.CyNetworkViewManager;
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.swing.AbstractCyAction;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.model.CyNetworkViewManager;
 
 /**
  * Base class for all NetworkAnalyzer actions which operate on a single network.
@@ -60,9 +34,9 @@ public abstract class NetAnalyzerAction extends AbstractCyAction {
 	}
 
 	protected NetAnalyzerAction(final String aName, final CyApplicationManager appMgr,
-			final Map<String, String> configProps,
+			final Map<String,String> configProps,
 			final CyNetworkViewManager networkViewManager) {
-		super(configProps, appMgr, networkViewManager);
+		super( configProps, appMgr, networkViewManager);
 		this.applicationManager = appMgr;
 		network = null;
 	}
@@ -83,7 +57,6 @@ public abstract class NetAnalyzerAction extends AbstractCyAction {
 	 * @return <code>true</code> if a network targeting analysis has been
 	 *         identified, <code>false</code> otherwise.
 	 */
-	@SuppressWarnings("fallthrough")
 	protected boolean selectNetwork() {
 		network = applicationManager.getCurrentNetwork();
 		if (network == null) {
